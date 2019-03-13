992060cd3a08f6ff33166464070ced0cb59afa62
jthicks91 <jordanthicks91@gmail.com>
2019-03-12 21:32:44 -0700

    added dummyData to App.js file

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+import dummyData from "./dummy-data";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dummyData: dummyData
+    };
+  }
   render() {
     return (
       <div className="App">
